56fac74d64068ff3d5557ca30d4b3928fed6fded
Scott Silverman <scottms724@gmail.com>
2018-06-20 15:54:50 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,7 +20,6 @@ class Scraper
   def get_page
     Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     end 
-  end 
   
   def get_courses 
     self.get_page.css(".post")
